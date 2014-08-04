3144e55eabb06d53710101cb91d98bfe18f0cede
Thomas Merkel <tm@core.io>
2014-08-04 19:53:14 +0200

    add uwsgi and ssl stuff

--- copy/opt/core/var/mdata-setup/includes/20-kumquat.sh
+++ copy/opt/core/var/mdata-setup/includes/20-kumquat.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 # Configure kumquat
 
-HOSTNAME=$(hostname)
+host=$(mdata-get sdc:hostname)
+cert_dir='/opt/local/etc/httpd/ssl/'
 
 # Setup hostname for kumquat config
-sed -i "s:HOSTNAME:${HOSTNAME}:g" /opt/local/etc/httpd/01-kumquat.conf
+sed -i "s:HOSTNAME:${host}:g" /opt/local/etc/httpd/01-kumquat.conf
+
+# SSL configuration
+if mdata-get kumquat_ssl 1>/dev/null 2>&1; then
+	mdata-get kumquat_ssl > ${cert_dir}/kumquat.pem
+else
+	openssl req -newkey rsa:2048 -keyout ${cert_dir}/kumquat.key \
+	            -out ${cert_dir}/kumquat.csr -nodes \
+	            -subj "/C=DE/L=Raindbow City/O=Aperture Science/OU=Please use valid ssl certificate/CN=${host}"
+	openssl x509 -in ${cert_dir}/kumquat.csr -out ${cert_dir}/kumquat.crt -req -signkey test.key -days 128
+fi
