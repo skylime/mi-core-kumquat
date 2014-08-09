9b53a09be46fc7d4aeaf033dd857bca4b28266cf
Thomas Merkel <tm@core.io>
2014-08-09 12:26:19 +0200

    fix path to ssl

--- copy/opt/core/var/mdata-setup/includes/20-kumquat.sh
+++ copy/opt/core/var/mdata-setup/includes/20-kumquat.sh
@@ -9,10 +9,10 @@ sed -i "s:HOSTNAME:${host}:g" /opt/local/etc/httpd/01-kumquat.conf
 
 # SSL configuration
 if mdata-get kumquat_ssl 1>/dev/null 2>&1; then
-	mdata-get kumquat_ssl > ${cert_dir}/kumquat.pem
+	mdata-get kumquat_ssl > ${cert_dir}kumquat.pem
 else
-	openssl req -newkey rsa:2048 -keyout ${cert_dir}/kumquat.key \
-	            -out ${cert_dir}/kumquat.csr -nodes \
+	openssl req -newkey rsa:2048 -keyout ${cert_dir}kumquat.key \
+	            -out ${cert_dir}kumquat.csr -nodes \
 	            -subj "/C=DE/L=Raindbow City/O=Aperture Science/OU=Please use valid ssl certificate/CN=${host}"
-	openssl x509 -in ${cert_dir}/kumquat.csr -out ${cert_dir}/kumquat.crt -req -signkey kumquat.key -days 128
+	openssl x509 -in ${cert_dir}kumquat.csr -out ${cert_dir}kumquat.crt -req -signkey ${cert_dir}kumquat.key -days 128
 fi
