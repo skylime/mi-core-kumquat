090fea911d11f316031eb2f8c3401ec49b33ea65
Thomas Merkel <tm@core.io>
2014-08-11 23:09:49 +0200

    sync mysql data and create user

--- copy/var/zoneinit/includes/50-kumquat.sh
+++ copy/var/zoneinit/includes/50-kumquat.sh
@@ -11,6 +11,9 @@ mdata-put mysql_kumquat_pw "${MYSQL_KUMQUAT}"
 ALLOWED_HOST=$(hostname)
 SECRET_KEY=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-100})
 
+ADMIN_KUMQUAT=${ADMIN_KUMQUAT:-$(mdata-get kumquat_admin 2>/dev/null)} || \
+ADMIN_KUMQUAT=$(od -An -N8 -x /dev/random | head -1 | tr -d ' ');
+
 # check if database exists already
 if [ -d /var/mysql/kumquat ]; then
 	echo 'Database already exists, maybe some migration script will run in the future.'
@@ -41,7 +44,7 @@ cat >> /opt/kumquat/kumquat_web/settings.py <<EOF
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = "${SECRET_KEY}"
 
-# Allow only the hostname and localhost to access
+# Allow only the hostname and localhost to access
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '${ALLOWED_HOST}']
 
 # Database
@@ -72,3 +75,10 @@ KUMQUAT_USE_ZFS          = ${USE_ZFS}
 KUMQUAT_VHOST_DATASET    = "${VHOST_DATASET}"
 KUMQUAT_WEBSERVER_RELOAD = 'svcadm refresh apache'
 EOF
+
+# Init django data and create admin user
+/opt/kumquat/manage.py syncdb --noinput
+
+# Create superadmin user
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', admin@example.com', '${ADMIN_KUMQUAT}')" \
+	| /opt/kumquat/manage.py shell
