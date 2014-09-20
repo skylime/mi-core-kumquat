5e7447b8fa53bdd0960e80f66d41afc15ef93c4a
Thomas Merkel <tm@core.io>
2014-09-20 15:25:55 +0200

    import sys is missing

--- copy/tmp/kumquat-settings.py
+++ copy/tmp/kumquat-settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
+import sys, os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # SECURITY WARNING: don't run with debug turned on in production!
