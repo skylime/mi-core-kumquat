67d5f607a565cda289750552d6028d94ce973e1b
Thomas Merkel <tm@core.io>
2014-10-06 22:06:46 +0200

    new: use logadm for proftpd logfile

--- copy/var/zoneinit/includes/51-proftpd.sh
+++ copy/var/zoneinit/includes/51-proftpd.sh
@@ -7,5 +7,9 @@ MYSQL_KUMQUAT=$(mdata-get mysql_kumquat_pw)
 	-e "s:WWW_UID:${WWW_UID}:g" -e "s:WWW_GID:${WWW_GID}:g" \
 	/opt/local/etc/proftpd.conf
 
+# Use logadm for rotate the logfiles
+logadm -w /var/log/proftpd/proftpd.log -p 1d -C 10 -N -m 640 -c
+logadm -w /var/log/proftpd/xferlog -p 1d -C 10 -N -m 640 -c
+
 # Enable proftpd service
 /usr/sbin/svcadm enable svc:/pkgsrc/proftpd:default
