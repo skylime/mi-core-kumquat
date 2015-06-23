843798c1fb9988eb73322b6bf052d376d76c5d5c
Thomas Merkel <tm@core.io>
2015-06-23 14:01:16 +0200

    Create snapshots for data and databases automatically

--- copy/var/zoneinit/includes/05-delegated-dataset.sh
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -9,3 +9,10 @@ if zfs list ${DDS} 1>/dev/null 2>&1; then
 	zfs set mountpoint=/var/www ${DDS}/www
 	zfs set mountpoint=/var/mysql ${DDS}/mysql
 fi
+
+# znapzend for backup
+znapzendzetup create --recursive --tsformat='%Y-%m-%d-%H%M%S' --donotask \
+	SRC '7day=>8hour,30day=>1day,1year=>1week,10year=>1month' ${DDS}/www
+znapzendzetup create --recursive --tsformat='%Y-%m-%d-%H%M%S' --donotask \
+	SRC '7day=>8hour,30day=>1day,1year=>1week,10year=>1month' ${DDS}/mysql
+/usr/sbin/svcadm enable svc:/pkgsrc/znapzend:default
