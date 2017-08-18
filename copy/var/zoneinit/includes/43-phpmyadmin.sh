800ff337d4a4adf7222bc52ee22987dde7e363bf
Thomas Merkel <tm@core.io>
2017-08-18 14:35:34 +0200

    Add more secure / longer blowfish secret

--- copy/var/zoneinit/includes/43-phpmyadmin.sh
+++ copy/var/zoneinit/includes/43-phpmyadmin.sh
@@ -1,7 +1,7 @@
 # Modify phpMyAdmin configuration file and kumquat menu
 
 # Create blowfish secret for phpMyAdmin
-GEN_BLOWFISH_SECRET=$(od -An -N8 -x /dev/random | head -1 | sed 's/^[ \t]*//' | tr -d ' ')
+GEN_BLOWFISH_SECRET=$(od -An -N64 -x /dev/random | head -1 | sed 's/^[ \t]*//' | tr -d ' ')
 /opt/local/bin/sed -i -e "s:GEN_BLOWFISH_SECRET:${GEN_BLOWFISH_SECRET}:g" \
 	/opt/local/etc/phpmyadmin/config.inc.php
 
