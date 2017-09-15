26c78d68c5e5c038a0646bc903e1fd7de80da3c1
Thomas Merkel <tm@core.io>
2017-09-15 15:02:12 +0200

    Run znapzendzetup only once for the delegate dataset but recursive

--- copy/var/zoneinit/includes/05-delegated-dataset.sh
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -15,9 +15,7 @@ if zfs list ${DDS} 1>/dev/null 2>&1; then
 
 	# znapzend for backup
 	znapzendzetup create --recursive --tsformat='%Y-%m-%d-%H%M%S' --donotask \
-		SRC '2day=>8hour,14day=>1day,1year=>1month,10year=>1year' ${DDS}/www
-	znapzendzetup create --recursive --tsformat='%Y-%m-%d-%H%M%S' --donotask \
-		SRC '2day=>8hour,14day=>1day,1year=>1month,10year=>1year' ${DDS}/mysql
+		SRC '2day=>8hour,14day=>1day,1year=>1month,10year=>1year' ${DDS}
 
 	/usr/sbin/svcadm enable svc:/pkgsrc/znapzend:default
 fi
