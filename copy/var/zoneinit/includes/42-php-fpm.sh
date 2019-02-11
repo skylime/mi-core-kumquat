2854e3444df747a560b691047cda0139230ee5da
Thomas Merkel <tm@core.io>
2019-02-11 20:03:23 +0100

    Fix kumquat_admin_email for php-fpm.d value and path

--- copy/var/zoneinit/includes/42-php-fpm.sh
+++ copy/var/zoneinit/includes/42-php-fpm.sh
@@ -3,7 +3,7 @@
 # Configure PHP sendmail return-path if possible
 if mdata-get kumquat_admin_email 1>/dev/null 2>&1; then
 	echo "php_admin_value[sendmail_path] = /usr/sbin/sendmail -t -i -f $(mdata-get kumquat_admin_email)" \
-		>> /opt/local/etc/fpm.d/pool-www.conf
+		>> /opt/local/etc/php-fpm.d/pool-www.conf
 fi
 
 # Enable PHP-FPM
