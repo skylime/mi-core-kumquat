3c60b639781d976aa1944b8b9b30e32d0a452db1
Thomas Merkel <tm@core.io>
2021-10-12 13:50:47 +0200

    Only create znapzendzetup when it's not already enabled
    `znapzendzetup create` overwrites all old configurations and if we've
    already configured some remote it should not be created again.

--- copy/opt/core/var/postinit/includes/10-znapzendzetup.sh
+++ copy/opt/core/var/postinit/includes/10-znapzendzetup.sh
@@ -4,9 +4,12 @@ DDS=zones/${UUID}/data
 log "check delegate dataset ${DDS}"
 
 if zfs list ${DDS} 1>/dev/null 2>&1; then
-        log "znapzend for backup"
-        znapzendzetup create --recursive --tsformat='%Y-%m-%d-%H%M%S' --donotask \
-                SRC '2day=>8hour,14day=>1day,1year=>1month,10year=>1year' ${DDS}
+
+        if [[ "$(zfs get -H -o value org.znapzend:enabled ${DDS} 2>&1)" != "on" ]]; then
+                log "znapzend for backup"
+                znapzendzetup create --recursive --tsformat='%Y-%m-%d-%H%M%S' --donotask \
+                        SRC '2day=>8hour,14day=>1day,1year=>1month,10year=>1year' ${DDS}
+        fi
 
         /usr/sbin/svcadm enable svc:/pkgsrc/znapzend:default
 fi
