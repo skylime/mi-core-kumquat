74941004bd1913862c50b5659e76a025af7e62a7
Thomas Merkel <tm@core.io>
2025-08-26 21:21:48 +0200

    Version bump to 2024Q4 with PHP version 8.3 and MariaDB 11.4
    * Switching to Python 3.12
    * Switching to PHP 8.3
    * Switching to MariaDB 11.4

--- copy/opt/core/var/postinit/includes/20-check-mysql-upgrade.sh
+++ copy/opt/core/var/postinit/includes/20-check-mysql-upgrade.sh
@@ -4,21 +4,21 @@
 export HOME=/root
 
 if [ -f /var/mysql/mysql_upgrade_info ]; then
-	# mysql_upgrade_info exists, so version check is required
-	mysql_select_version=$(mysql -e 'select version();' --skip-column-names)
-	running_version=${mysql_select_version%-*}
-	upgrade_info_version=$(cat /var/mysql/mysql_upgrade_info)
-	if [[ "${running_version}" == "${upgrade_info_version}" ]]; then
-		# Running version and upgrade information are the same. No
-		# uprade required.
-		exit 0
-	fi
+  # mysql_upgrade_info exists, so version check is required
+  mysql_select_version=$(mysql -e 'select version();' --skip-column-names 2> /dev/null || echo "0")
+  running_version=${mysql_select_version%-*}
+  upgrade_info_version=$(cat /var/mysql/mysql_upgrade_info)
+  if [[ "${running_version}" == "${upgrade_info_version}" ]]; then
+    # Running version and upgrade information are the same. No
+    # uprade required.
+    exit 0
+  fi
 fi
 
 # mysql_upgrade_info doesn't exists or upgrade information are different to
 # local running version. Information added to /etc/motd
 cp /etc/motd /etc/motd.clean
-cat >> /etc/motd <<-EOF
+cat >> /etc/motd <<- EOF
  ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
  ┃ Operation task recommended:                                        ┃
  ┃    $ mysql_upgrade && svcadm restart svc:/pkgsrc/mariadb:default   ┃
