5ce9b5a8b7488d070f44a2ab319fb855a7d6129f
Thomas Merkel <tm@core.io>
2026-09-07 23:17:53 +0200

    fix: php-fpm config generation

--- copy/var/zoneinit/includes/42-php-fpm.sh
+++ copy/var/zoneinit/includes/42-php-fpm.sh
@@ -7,7 +7,7 @@ if mdata-get kumquat_admin_email 1> /dev/null 2>&1; then
 fi
 
 # Configure default pm for PHP-FPM (3 pools exists, ~40 MB per worker expected)
-/opt/core/bin/php-fpm-settings-gen.sh -p 3 -w 40 >> /opt/local/etc/php/{7.4,8.2,8.4}/php-fpm.d/www.conf
+/opt/core/bin/php-fpm-settings-gen.sh -p 3 -w 40 | tee -a /opt/local/etc/php/{7.4,8.2,8.4}/php-fpm.d/www.conf
 
 # Enable PHP-FPM
 /usr/sbin/svcadm enable svc:/pkgsrc/php-fpm74:default
